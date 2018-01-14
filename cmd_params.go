27c29496a96024f7225c265ba01fcdfb9c013076
abtv <andreybutov83@gmail.com>
2018-01-14 13:02:00 +0300

    add command to CmdParams

--- cmd_params.go
+++ cmd_params.go
@@ -3,14 +3,21 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
+	"os"
 )
 
 type CmdParams struct {
+	command   string
 	pattern   string
 	filenames []string
 }
 
 func getCmdParams() (CmdParams, error) {
+	args := os.Args[1:]
+
+	fmt.Println(args)
+
 	pattern := flag.String("pattern", "", "pattern prefix to search")
 	filename := flag.String("file", "", "name of file")
 	flag.Parse()
